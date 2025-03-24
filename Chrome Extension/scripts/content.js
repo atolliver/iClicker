0bb675e34e383c4a96016575690ba1bb77022061
Alex Tolliver <112031812+atolliver@users.noreply.github.com>
2025-03-24 17:38:18 -0500

    Updated content.js to properly detect question change

--- Chrome Extension/scripts/content.js
+++ Chrome Extension/scripts/content.js
@@ -1,27 +1,47 @@
-const targetNode = document.querySelector(".center-buttons h1");
+function elementChecker(selector, timeout = 10000) {
+  //timeout = 10 minutes
+  return new Promise((resolve, reject) => {
+    const pollingRate = 100;
+    let timeElapsed = 0;
 
-const observer = new MutationObserver((mutatationsList) => {
-  for (const mutation of mutatationsList) {
-    if (
-      mutation.type === "childList" ||
-      mutation.type === "characterData" ||
-      mutation.type === "subtree"
-    ) {
-      console.log("Change detected: ", mutation);
-      console.log("New content: ", targetNode.textContent.trim());
-    }
-  }
-});
+    const interval = setInterval(() => {
+      const element = document.querySelector(selector);
+      if (element) {
+        clearInterval(interval);
+        resolve(element);
+      } else if ((timeElapsed += pollingRate) <= timeout) {
+        clearInterval(interval);
+        reject(new Error("Could not find the element within timeout"));
+      }
+    }, pollingRate);
+  });
+}
 
-const config = {
-  childList: true,
-  characterData: true,
-  subtree: true,
-};
+elementChecker(".center-buttons h1")
+  .then((targetNode) => {
+    const observer = new MutationObserver((mutationsList) => {
+      for (const mutation of mutationsList) {
+        if (
+          mutation.type === "childList" ||
+          mutation.type === "characterData" ||
+          mutation.type === "subtree"
+        ) {
+          console.log("Change detected: ", mutation);
+          console.log("New content: ", targetNode.textContent.trim());
+        }
+      }
+    });
 
-observer.observe(targetNode, config);
-console.log("Observing started!");
+    const config = {
+      childList: true,
+      characterData: true,
+      subtree: true,
+    };
 
-const timeoutMultiplier = 2; // x minutes
-setTimeout(observer.disconnect(), timeoutMultiplier * 1000);
+    observer.observe(targetNode, config);
+    console.log("Observing started!");
 
+    const timeoutMultiplier = 120; // x seconds
+    setTimeout(() => observer.disconnect(), timeoutMultiplier * 1000);
+  })
+  .catch((error) => console.warn("Mutation observer error: ", error));
